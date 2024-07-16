b33fb6d5a475801c157eab00b725617ca69e977c
Tobias Heinzmann <mail@tobias-heinzmann.de>
2024-07-16 16:25:14 +0200

    adds get_data and reduced setup.py

--- cybnews/data.py
+++ cybnews/data.py
@@ -1,23 +1,6 @@
-#!/usr/bin/env python3
-
 import pandas as pd
-from pathlib import Path
-from os.path import abspath
 
 
-def get_data():
-    """
-    This function returns a Python dict.
-    Its keys should be 'sellers', 'orders', 'order_items' etc...
-    Its values should be pandas.DataFrames loaded from csv files
-    """
-    data = {}
+def get_data(path):
+    return pd.read_csv()
 
-    project_path = Path(abspath(__file__)).parent.parent
-    csv_path = project_path.joinpath('data', 'WELFake_Dataset.csv')
-
-    return pd.read_csv(csv_path)
-
-
-if __name__ == '__main__':
-    get_data()
