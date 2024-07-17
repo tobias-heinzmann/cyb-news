cc460147e1b6cf78b720aaa165c7d7f7400f18aa
Tobias Heinzmann <mail@tobias-heinzmann.de>
2024-07-17 23:20:52 +0200

    adding isot data set preprocessing

--- cybnews/data.py
+++ cybnews/data.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import pandas as pd
 import string
 
@@ -8,44 +6,71 @@ from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
 
+LANGUAGE='english'
+STOPWORDS_DEL = ["not", "no", "nor", "against", "however", "but", "never", "should", "would" , "could", "might", "must", "no", "yes", "always", "none", "only", "still", "yet", "despite", "unless", "until", "cannot" ]
+
+
 def get_data(path):
     return pd.read_csv(path)
 
 
-def join_text_welf(data: pd.DataFrame) -> pd.DataFrame:
-    data = data.fillna('')
-    data["all_text"] = data["title"] + " " +  data["text"]
-    return data
-
-
-def _preprocessing(sentence):
+def tokenize(sentence):
     sentence = sentence.strip()
     sentence = sentence.lower()
     sentence = "".join(char for char in sentence if not char.isdigit())
     
     for x in string.punctuation:
-        sentence = sentence.replace(x, "")
+        sentence = sentence.replace(x, '')
 
-    for x in ["’", "“", "”", "-" ]:
-        sentence = sentence.replace(x, "")
+    for x in ['’', '“', '”', '-', '"' ]:
+        sentence = sentence.replace(x, '')
 
-    tokens = word_tokenize(sentence)
+    return word_tokenize(sentence)
 
-    language = set(stopwords.words("english"))
-    #language.update(["trump", "clinton", "obama"])
-    language.difference_update(["not", "no","nor", "against", "however", "but", "never", "should", "would" , "could", "might", "must", "no", "yes", "always", "none", "only", "still", "yet", "despite", "unless", "until", "cannot" ])
-    sentence = [w for  w in tokens if not w in language]
-    v_l = [WordNetLemmatizer().lemmatize(word, pos = "v") for word in sentence]
+
+def clear_stopwords(tokens, stopw_add=[], stopw_del=[], lang=LANGUAGE):
+    stop_words = set(stopwords.words(lang))
+    stop_words.update(stopw_add)
+    stop_words.difference_update(stopw_del)
+
+    return [w for w in tokens if w not in stop_words]
+    
+
+def preprocessing(sentence):
+    tokens = tokenize(sentence)
+    clean_tokens = clear_stopwords(tokens, stopw_del=STOPWORDS_DEL)
+    
+    v_l = [WordNetLemmatizer().lemmatize(word, pos = "v") for word in clean_tokens]
     n_l = [WordNetLemmatizer().lemmatize(word, pos = "n") for word in v_l]
     return ' '.join(n_l)
 
 
-def preprocessing(data: pd.DataFrame) -> pd.DataFrame:
-    data["all_text_cleaned"] = data["all_text"].apply(_preprocessing)
-    data_preprocessed = data[["all_text_cleaned", "label"]]
-    return data_preprocessed
-
-
 def preprocess_input(text: str):
-    return [_preprocessing(text)]
-    
+    return [preprocessing(text)]
+
+
+
+### WELF data set specific
+#
+def welf_join_text(data: pd.DataFrame):
+    data = data.fillna('')
+    data["all_text"] = data["title"] + " " +  data["text"]
+    return data
+
+
+def welf_preprocessing(data: pd.DataFrame):
+    data["all_text_cleaned"] = data["all_text"].apply(preprocessing)
+    return data[["all_text_cleaned", "label"]]
+
+
+### ISOT data set specifiv
+#
+def isot_join_text(data: pd.DataFrame):
+    """ISOT also has 'tilte' and 'text' columns..."""
+    data["all_text"] = data["title"] + " " +  data["text"]
+    return data
+
+
+def isot_preprocessing(data: pd.DataFrame):
+    data["all_text_cleaned"] = data["all_text"].apply(preprocessing)
+    return data[["all_text_cleaned"]]
