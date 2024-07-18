a66e1d069f6d83a7c2e83eb762cafc845bebf80e
Frederik Mentner <frederik.mentner@gmx.de>
2024-07-18 13:09:07 +0200

    Added json requrement | coded function to preprocess, build and save worldcloud json dicts

--- cybnews/data.py
+++ cybnews/data.py
@@ -4,9 +4,9 @@ import string
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
-from wordcloud import WordCloud
-from collections import Counter
 
+from collections import Counter
+import json
 LANGUAGE='english'
 STOPWORDS_DEL = ["not", "no", "nor", "against", "however", "but", "never", "should", "would" , "could", "might", "must", "no", "yes", "always", "none", "only", "still", "yet", "despite", "unless", "until", "cannot" ]
 STOPWORDS_ADD = ["reuters"]
@@ -84,9 +84,65 @@ def isot_preprocessing(data: pd.DataFrame):
     return data[["all_text_cleaned"]]
 
 
-def get_wordcloud():
+
+
+
+# Build Dict for Wordcloud
+
+def get_word_count(sentence):
+    tokens = tokenize(sentence)
+    v_l = [WordNetLemmatizer().lemmatize(word, pos = "v") for word in tokens]
+    lemmatized_token = [WordNetLemmatizer().lemmatize(word, pos = "n") for word in v_l]
+    clean_tokens = clear_stopwords(
+        lemmatized_token,
+        stopw_del=STOPWORDS_DEL,
+        stopw_add=STOPWORDS_ADD
+    )
+    word_counts = Counter(clean_tokens)
+    return word_counts
+
+
+
+
+def get_wordcloud(data: pd.DataFrame):
+    data["counted"] = data["all_text"].apply(get_word_count)
+    real_news= data[data["label"] == 0]
+    fake_news= data[data["label"] == 1]
+    total_word_counts_fake = Counter()
+
+    for counts in fake_news["counted"]:
+        total_word_counts_fake.update(counts)
+    total_fake = sum(total_word_counts_fake.values())
+
+    total_word_counts_real = Counter()
+    for counts in real_news["counted"]:
+        total_word_counts_real.update(counts)
+    total_real = sum(total_word_counts_real.values())
+
+    relative_freq_fake = {word: count / total_fake for word, count in total_word_counts_fake.items()}
+    relative_freq_real = {word: count / total_real for word, count in total_word_counts_real.items()}
+
+    all_words = set(relative_freq_fake.keys()).union(set(relative_freq_real.keys()))
+    diffs = {word: relative_freq_fake.get(word, 0) - relative_freq_real.get(word, 0) for word in all_words}
+
+
+    fake_uptop = dict(sorted(diffs.items(), key=lambda item: item[1], reverse=True))
+    fake_uptop_list = list(fake_uptop.items())
+    fake_uptop_list_200 = fake_uptop_list[:200]
+    words_overrepresented_in_fakes = dict(fake_uptop_list_200)
+
+    fake_uptop_revesed = {word: -value for word, value in fake_uptop.items()}
+    real_uptop= dict(sorted(fake_uptop_revesed.items(), key=lambda item: item[1], reverse=True))
+    real_uptop_list = list(real_uptop.items())
+    real_uptop_list_200 = real_uptop_list[:200]
+    words_overrepresented_in_real = dict(real_uptop_list_200)
+
+
     return words_overrepresented_in_real, words_overrepresented_in_fakes # dict
 
 
 def save_wordcloud(dict_real, dict_fake):
-    pass # pickeld dict in .json /.txt
+    with open('words_real.json', 'w') as json_file:
+        json.dump(dict_real, json_file)
+    with open('words_fale.json', 'w') as json_file:
+        json.dump(dict_fake, json_file)
