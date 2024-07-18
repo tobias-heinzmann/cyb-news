4c7e8f12beeed31bf4fde82093b68637a54c71d8
Frederik Mentner <frederik.mentner@gmx.de>
2024-07-18 14:24:57 +0200

    Adjusted wordcloud function | got filepath for try in data.py

--- cybnews/data.py
+++ cybnews/data.py
@@ -10,7 +10,8 @@ import json
 LANGUAGE='english'
 STOPWORDS_DEL = ["not", "no", "nor", "against", "however", "but", "never", "should", "would" , "could", "might", "must", "no", "yes", "always", "none", "only", "still", "yet", "despite", "unless", "until", "cannot" ]
 STOPWORDS_ADD = ["reuters"]
-
+MODEL_PATH = "/Users/admin/code/frederiklm/cyb-news/models"
+DATA_PATH = "/Users/admin/code/frederiklm/cyb-news/data/WELFake_Dataset.csv"
 
 def get_data(path):
     return pd.read_csv(path)
@@ -89,6 +90,8 @@ def isot_preprocessing(data: pd.DataFrame):
 
 # Build Dict for Wordcloud
 
+
+
 def get_word_count(sentence):
     tokens = tokenize(sentence)
     v_l = [WordNetLemmatizer().lemmatize(word, pos = "v") for word in tokens]
@@ -102,9 +105,8 @@ def get_word_count(sentence):
     return word_counts
 
 
-
-
-def get_wordcloud(data: pd.DataFrame):
+def get_word_cloud(data: pd.DataFrame):
+    data = welf_join_text(data)
     data["counted"] = data["all_text"].apply(get_word_count)
     real_news= data[data["label"] == 0]
     fake_news= data[data["label"] == 1]
@@ -125,7 +127,6 @@ def get_wordcloud(data: pd.DataFrame):
     all_words = set(relative_freq_fake.keys()).union(set(relative_freq_real.keys()))
     diffs = {word: relative_freq_fake.get(word, 0) - relative_freq_real.get(word, 0) for word in all_words}
 
-
     fake_uptop = dict(sorted(diffs.items(), key=lambda item: item[1], reverse=True))
     fake_uptop_list = list(fake_uptop.items())
     fake_uptop_list_200 = fake_uptop_list[:200]
@@ -136,13 +137,23 @@ def get_wordcloud(data: pd.DataFrame):
     real_uptop_list = list(real_uptop.items())
     real_uptop_list_200 = real_uptop_list[:200]
     words_overrepresented_in_real = dict(real_uptop_list_200)
-
+    print(words_overrepresented_in_real)
 
     return words_overrepresented_in_real, words_overrepresented_in_fakes # dict
 
 
-def save_wordcloud(dict_real, dict_fake):
-    with open('words_real.json', 'w') as json_file:
+def save_wordcloud(dict_real, dict_fake, model_path=MODEL_PATH):
+    with open(f'{model_path}/words_real.json', 'w') as json_file:
         json.dump(dict_real, json_file)
-    with open('words_fale.json', 'w') as json_file:
+    with open(f'{model_path}/words_fake.json', 'w') as json_file:
         json.dump(dict_fake, json_file)
+
+
+
+
+
+if __name__ == '__main__':
+    data = get_data(DATA_PATH)
+    data= welf_join_text(data)
+    real, fake = get_word_cloud(data)
+    save_wordcloud(real,fake, MODEL_PATH)
--- cybnews/main.py
+++ cybnews/main.py
@@ -7,13 +7,19 @@ DATA_PATH = "/Users/admin/code/frederiklm/cyb-news/data/WELFake_Dataset.csv"
 
 if __name__ == "__main__":
     data = get_data(DATA_PATH)
+    print("let")
     data = welf_join_text(data)
+    print("us")
     #data = data.sample(frac=0.01, random_state=42)
     data = welf_preprocessing(data)
+    print("wait")
     X_train, X_test, y_train, y_test  = train_test_split_data(data)
+    print("for")
     model = create_new_model(X_train, y_train)
+    print("ever")
     #model = load_model('model.pkl', MODEL_PATH)
     save_model(model, MODEL_PATH)
+    print("!")
     pred = model.predict(preprocess_input('sadfasdfs v dfgesgxwe'))
     pred_proba = model.predict_proba(preprocess_input('sadfasdfs v dfgesgxwe'))
 
